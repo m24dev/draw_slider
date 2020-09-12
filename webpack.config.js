a8eeb65d42779484249686de8896434ef552a627
Alex <alexilin83@gmail.com>
2020-09-12 11:13:12 +0300

    first demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -10,7 +11,8 @@ module.exports = env => {
             main: './src/js/index.js'
         },
         output: {
-            filename: 'js/[name].[contenthash].js'
+            filename: 'js/[name].[contenthash].js',
+            path: path.resolve(__dirname, 'docs')
         },
         devtool: isDev ? 'cheap-module-source-map' : 'none',
         mode,
